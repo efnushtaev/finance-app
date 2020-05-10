dc9fc7346c46ae0e4eae5bb2eaa124826f4e7131
efnushtaev <efnushtaev@gmail.com>
2020-05-10 21:14:39 +0300

    addNewObservableStock action modified

--- src/redux/reducers/stockReducer.js
+++ src/redux/reducers/stockReducer.js
@@ -18,10 +18,10 @@ let initialState = {
             stockName: '',
             stockData: '',
             stockValue: '',
-            stochasticValue: '',
-            stochasticRsi: '',
             priceDifferValue: '',
-            advice: ''
+            advice: '',
+            stochasticValue: '',
+            stochasticRsi: ''
         }
     ],
     watchList: [],
@@ -37,43 +37,57 @@ export const stockReducer = (state = initialState, action) => {
         case ADD_NEW_OBSERVABLE_STOCK: {
             return {
                 ...state,
-                observableStock: [...state.observableStock, action.payload],
-                currentStock: action.payload
+                observableStock: [...state.observableStock, {
+                    stockName: action.stockName,
+                    stockData: action.stockData,
+                    stockValue: action.stockValue,
+                    priceDifferValue: action.priceDifferValue,
+                    advice: '',
+                    stochasticValue: '',
+                    stochasticRsi: ''
+                }]
             }
         }
-        case UPDATE_CURRENT_DATA_STOCK: {
-            return {
-                ...state,
-                stockData: [...action.payload]
-            }
-        }
-        case SET_PRICE_DIFFERENCE: {
-            return {
-                ...state,
-                priceDifferenceForPeriod: action.payload
-            }
-        }
+        // case UPDATE_CURRENT_DATA_STOCK: {
+        //     return {
+        //         ...state,
+        //         observableStock: [...state.observableStock, action.payload],
+        //         stockData: [...action.payload]
+        //     }
+        // }
+        // case SET_PRICE_DIFFERENCE: {
+        //     return {
+        //         ...state,
+        //         priceDifferenceForPeriod: action.payload
+        //     }
+        // }
 
         default:
             return state
     }
 }
 
-export const addNewObservableStock = payload => ({type: ADD_NEW_OBSERVABLE_STOCK, payload})
+export const addNewObservableStock = (stockName, stockData, stockValue, priceDifferValue) => ({type: ADD_NEW_OBSERVABLE_STOCK, stockName, stockData, stockValue, priceDifferValue})
 export const updateCurrentDataStock = payload => ({type: UPDATE_CURRENT_DATA_STOCK, payload})
 export const setPriceDifference = payload => ({type:SET_PRICE_DIFFERENCE, payload})
+
 
 let newStock = new StockInstance()
 
 export const addStock = ( stockFunction, currentStock, apikey ) => (dispatch) => {
     StockAPI.getStock( stockFunction, currentStock, apikey )
-    .then( response => {
-        if ('Error Message' in response) {
+    .then( res => {
+        if ('Error Message' in res) {
             return 'error'
         } else {
-            dispatch(addNewObservableStock(newStock.getStockName(response)))
-            dispatch(updateCurrentDataStock(newStock.getStockData(response)));
-            dispatch(setPriceDifference(newStock.getPriceDifferValue(response)))
+            dispatch(addNewObservableStock(
+                newStock.getStockName(res), 
+                newStock.getStockData(res), 
+                newStock.getStockValue(res), 
+                newStock.getPriceDifferValue(res))
+            )
+            // dispatch(updateCurrentDataStock(newStock.getStockData(response)));
+            // dispatch(setPriceDifference(newStock.getPriceDifferValue(response)))
         }
     })
 

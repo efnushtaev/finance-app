ccea07be3c2a2d0cc165e1539cf6c1f20db95f31
efnushtaev <efnushtaev@gmail.com>
2020-07-19 12:25:51 +0300

    Updates

---
+++ precache-manifest.0c3256cfaabbb64479e49921b3272a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46beedc17814c758f1e0c9744a9be239",
+    "url": "/finance-app/index.html"
+  },
+  {
+    "revision": "482a25f82580ca3d4d36",
+    "url": "/finance-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7f04fb0ec12a1d87876",
+    "url": "/finance-app/static/js/2.3ccb66a1.chunk.js"
+  },
+  {
+    "revision": "c099810c3c102db7a44755001529c804",
+    "url": "/finance-app/static/js/2.3ccb66a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482a25f82580ca3d4d36",
+    "url": "/finance-app/static/js/main.9ed5e6a0.chunk.js"
+  },
+  {
+    "revision": "b5fd38c3941c17d68560",
+    "url": "/finance-app/static/js/runtime-main.673347b4.js"
+  }
+]);
